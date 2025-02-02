64d073486089a27c8e5d4e69bbc2ce348e269ce7
Connor <conkelley7@gmail.com>
2025-02-02 14:26:28 -0600

    Created Entity classes "Owner" and "Vehicle"
    Initialized entity classes for owners table and vehicles tables.

---
+++ autoregistry/src/main/java/com/kelley/autoregistry/model/Owner.java
@@ -0,0 +1,10 @@
+package com.kelley.autoregistry.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "owners")
+public class Owner {
+	// TODO: Create entity fields matching those in Database Diagram
+}
---
+++ autoregistry/src/main/java/com/kelley/autoregistry/model/Vehicle.java
@@ -0,0 +1,10 @@
+package com.kelley.autoregistry.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="vehicles")
+public class Vehicle {
+	// TODO: Create entity fields matching those in Database Diagram
+}
