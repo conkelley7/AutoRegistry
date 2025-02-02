76144e5418d5dec36c72e0d81617650732136b0e
Connor <conkelley7@gmail.com>
2025-02-02 17:09:11 -0600

    Created DTO classes for Vehicle and Owner.

---
+++ autoregistry/src/main/java/com/kelley/autoregistry/dto/OwnerDTO.java
@@ -0,0 +1,73 @@
+package com.kelley.autoregistry.dto;
+
+public class OwnerDTO {
+
+    private Long ownerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    public OwnerDTO() {
+    }
+
+    public OwnerDTO(Long ownerId, String firstName, String lastName, String email, String phoneNumber, String address) {
+        this.ownerId = ownerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    // Getters and Setters
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ autoregistry/src/main/java/com/kelley/autoregistry/dto/VehicleDTO.java
@@ -0,0 +1,106 @@
+package com.kelley.autoregistry.dto;
+
+import java.time.LocalDate;
+
+public class VehicleDTO {
+
+    private Long vehicleId;
+    private String vin;
+    private String make;
+    private String model;
+    private int year;
+    private String color;
+    private String licensePlate;
+    private Long ownerId;
+    private LocalDate registrationDate;
+
+    public VehicleDTO() {
+    }
+
+    public VehicleDTO(Long vehicleId, String vin, String make, String model, int year, String color, 
+                      String licensePlate, Long ownerId, LocalDate registrationDate) {
+        this.vehicleId = vehicleId;
+        this.vin = vin;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.licensePlate = licensePlate;
+        this.ownerId = ownerId;
+        this.registrationDate = registrationDate;
+    }
+
+    // Getters and Setters
+
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
