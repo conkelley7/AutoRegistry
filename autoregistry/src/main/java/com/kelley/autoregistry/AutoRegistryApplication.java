edcdb020d641f54de13fbe384d7d6abb85fffaef
Connor <conkelley7@gmail.com>
2025-02-02 14:20:11 -0600

    Initial commit

---
+++ autoregistry/src/main/java/com/kelley/autoregistry/AutoRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.kelley.autoregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoRegistryApplication.class, args);
+	}
+
+}
---
+++ autoregistry/src/test/java/com/kelley/autoregistry/AutoRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelley.autoregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
